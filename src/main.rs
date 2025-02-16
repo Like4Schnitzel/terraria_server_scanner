bd8466aa5d135d34c4f64c37dbb19f406a189e89
Schnitzel <rw21062006@gmail.com>
2025-02-16 22:53:57 +0100

    connect with timeout

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,19 @@
-use std::io::Read;
-use std::net::TcpStream;
-use terraria_protocol::net::{self, Terraria};
+use std::time::Duration;
+use terraria_protocol::net::Terraria;
+use iprange::IpRange;
 
-fn main() {    
-    let terraria = match Terraria::connect("62.46.80.167:7777") {
-        Ok(_) => {println!("Fortnite");},
-        Err(_) => {println!("Brawl Stars")},
+fn is_server_available(ip: &str, port: u16, timeout: Duration) -> bool {
+    let address = format!("{}:{}", ip, port);
+    let _ = match Terraria::connect_timeout(&address, timeout) {
+        Ok(_) => {
+            return true;
+        },
+        Err(_) => {
+            return false;
+        },
     };
+}
+
+fn main() {
+    println!("{}", is_server_available("62.46.80.167", 7777, Duration::from_secs(5)));
 }
