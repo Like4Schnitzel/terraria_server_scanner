2287643b42c02e52e613d947409f1871fbbc4300
Schnitzel <rw21062006@gmail.com>
2025-02-16 21:16:23 +0100

    implement server run checking

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
-fn main() {
-    println!("Hello, world!");
+use std::io::Read;
+use std::net::TcpStream;
+use terraria_protocol::net::{self, Terraria};
+
+fn main() {    
+    let terraria = match Terraria::connect("62.46.80.167:7777") {
+        Ok(_) => {println!("Fortnite");},
+        Err(_) => {println!("Brawl Stars")},
+    };
 }
